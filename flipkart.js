698c4319ffc4d1713da9014d83244732727e671f
Diwakar Sharma <diwakarsharma386@gmail.com>
2024-08-02 16:57:24 +0530

    Add New Files

---
+++ flipkart.js
@@ -0,0 +1,28 @@
+const searchButton = document.querySelector(".btn");
+const searchInput = document.querySelector(".search input");
+
+searchButton.addEventListener("click", () => {
+  const searchTerm = searchInput.value.trim();
+  if (searchTerm) {
+    alert(`You searched for: ${searchTerm}`);
+  } else {
+    alert("Please enter a search term.");
+  }
+});
+const slider = document.querySelector(".slider img");
+const images = [
+  "slider.png",
+  "slider2.webp",
+  "slider3.webp",
+  "slider4.webp",
+  "slider5.webp",
+  "slider6.webp",
+  "slider7.webp",
+];
+let currentIndex = 0;
+function changeImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  slider.src = images[currentIndex];
+}
+
+setInterval(changeImage, 3000);
